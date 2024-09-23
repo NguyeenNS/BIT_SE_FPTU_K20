dbc8850af0af1fff658785f8775062c6f9cc6d08
Nierux <89969112+NguyeenNS@users.noreply.github.com>
2024-09-23 12:40:51 +0700

    [092324] Added contact_keeper_template.c
    Something went wrong with this template so it's not working properly

---
+++ contact_kepper_template.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void print_menu()
+{
+	system("cls"); // cls -> win ; clear -> mac, linux
+	printf("CONTACT KEEPER\n");
+	printf("------------------------\n");
+	printf("1. Add new contact\n");
+	printf("2. Edit contact\n");
+	printf("3. Detele contact\n");
+	printf("0. Exit\n");
+}
+
+
+int main_choice(int min, int max)
+{
+	int not_valid = 1, num, scanf_ret;
+	char c;
+
+	print_menu();
+	fflush(stdin);
+	do
+	{
+		printf("\nEnter yyour choice: ");
+		scanf_ret = scanf("%d%c", &num, &c);
+		if (scanf_ret < 2 || c != '\n')
+		{
+			print_menu();
+			printf("\nYour choice is not valid. Please try again!\n");
+			fflush(stdin);
+		}
+		else if (num < min || num > max)
+		{
+			print_menu();
+			printf("\nYour choice is not valid. Please try again!\n");
+		}
+		else
+		{
+			not_valid = 0;
+		}
+	} while (not_valid);
+
+	system("cls");
+
+	return num;
+}
+
+
+void add_contact()
+{
+	printf("Write your code here to implement the add_contact() function.\n");
+}
+
+
+void edit_contact()
+{
+	printf("Write your code here to implement the edit_contact() function.\n");
+}
+
+
+void delete_contact()
+{
+	printf("Write your code here to implement the delete_contact() function.\n");
+}
+
+
+int main(void)
+{
+	void (*fun[4])(void) = {NULL, &add_contact, &edit_contact, &delete_contact};
+	int choice;
+
+	do
+	{
+		choice = make_choice(0, 3);
+		if (choice)
+		{
+			fun[choice]();
+			printf("\nPress any key to return to the menu.");
+			getchar();
+		}
+	}
+	while (choice);
+
+	return 0;
+}
